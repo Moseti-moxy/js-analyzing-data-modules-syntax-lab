888913e4498aa354d207d6dab2e8919d7964a875
Eugene <maseseeugene@gmail.com>
2026-04-15 14:56:15 +0300

    added files

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 
 
 
+function combineUsers(...args) {
+  // Step 2: Initialize the Return Object
+  const combinedObject = {
+    users: []
+  };
+
+  // Step 3: Loop through args
+  for (const userArray of args) {
+    // Step 4: Merge Arrays using spread operator
+    combinedObject.users.push(...userArray);
+  }
+
+  // Step 5: Get today's date
+  const today = new Date();
+  combinedObject.merge_date = today.toString('M/d/yyyy');
+
+  // Step 7: Return object
+  return combinedObject;
+}
+const group1 = ['Eugene','Mark']
+const group2 = ['charlie', 'diana'];
+const group3 = ['eve'];
+
+
+console.log(combineUsers(group1, group2, group3));
+
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
